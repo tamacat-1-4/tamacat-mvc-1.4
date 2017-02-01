1a287429665711be49819bb7a5f3683e284629a4
tamacat <tamacat@f3cb667f-c9e3-46e3-bc2c-302c0b7402fb>
2017-02-01 12:26:37 +0000

    add bindJsonArray change generics types.
    git-svn-id: https://svn.code.sf.net/p/tamacat/code/branches/tamacat-mvc-1.4@1133 f3cb667f-c9e3-46e3-bc2c-302c0b7402fb

--- src/main/java/org/tamacat/mvc/util/BeanUtils.java
+++ src/main/java/org/tamacat/mvc/util/BeanUtils.java
@@ -5,17 +5,20 @@
 package org.tamacat.mvc.util;
 
 import java.io.IOException;
+import java.util.Collection;
 import java.util.Map;
 
+import javax.json.Json;
 import javax.servlet.http.HttpServletRequest;
 
 import org.tamacat.dao.meta.Column;
-import org.tamacat.dao.orm.ORMappingSupport;
+import org.tamacat.dao.orm.MapBasedORMappingBean;
+import org.tamacat.dao.util.JSONUtils;
 import org.tamacat.util.CollectionUtils;
 
 public class BeanUtils {
 
-	public static <T> ORMappingSupport<? extends T> bind(ORMappingSupport<? extends T> bean, HttpServletRequest req, Column... columns) {
+	public static <T extends MapBasedORMappingBean<?>> MapBasedORMappingBean<?> bind(T bean, HttpServletRequest req, Column... columns) {
 		for (Column column : columns) {
 			String val = req.getParameter(column.getColumnName());
 			if (val != null) {
@@ -25,7 +28,7 @@ public class BeanUtils {
 		return bean;
 	}
 
-	public static <T> ORMappingSupport<? extends T> bind(ORMappingSupport<? extends T> bean, HttpServletRequest req, Column column, String name) {
+	public static <T extends MapBasedORMappingBean<?>> MapBasedORMappingBean<?> bind(T bean, HttpServletRequest req, Column column, String name) {
 		String val = req.getParameter(name);
 		if (val != null) {
 			bean.val(column, val);
@@ -33,7 +36,7 @@ public class BeanUtils {
 		return bean;
 	}
 	
-	public static <T> ORMappingSupport<? extends T> bindJson(ORMappingSupport<? extends T> bean, HttpServletRequest req, Column... columns) {
+	public static <T extends MapBasedORMappingBean<?>> MapBasedORMappingBean<?> bindJson(T bean, HttpServletRequest req, Column... columns) {
 		if (req.getContentType().startsWith("application/json")) {
 			Map<String, Column> colmaps = CollectionUtils.newLinkedHashMap();
 			for (Column col : columns) {
@@ -46,6 +49,22 @@ public class BeanUtils {
 			}
 		}
 		return bean;
+	}
+	
+	public static <T extends MapBasedORMappingBean<?>> Collection<T> bindJsonArray(HttpServletRequest req, Class<T> type, Column... columns) {
+		if (req.getContentType().startsWith("application/json")) {
+			Map<String, Column> colmaps = CollectionUtils.newLinkedHashMap();
+			for (Column col : columns) {
+				colmaps.put(col.getColumnName(), col);
+			}
+			try {
+				req.setCharacterEncoding("UTF-8");
+				return JSONUtils.parseArray(Json.createParser(req.getReader()), type, columns);
+			} catch (IOException e) {
+				//skip
+			}
+		}
+		return CollectionUtils.newArrayList();
 	}
 
 }
